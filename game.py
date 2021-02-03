5c6cb12e64c5633a8e682962bdc25f87e89e1a23
Alex_Kelly <afkelly14@Gmail.com>
2021-02-03 15:50:22 -0500

    checking whether there is 3 in a row

--- game.py
+++ game.py
@@ -8,20 +8,84 @@ class TicTacToe:
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
 
-@staticmethod
-def print_board_nums():
+    @staticmethod
+    def print_board_nums():
     # 0 | 1 | 2 etc (tells us what number corresponse to what box)
-    number_board = [[str(i) for i in range(j*3), (j+1)*3] for j in range(3)]
-    for row in number_board:
-        print('| ' + ' | '.join(row) + ' |')
+        number_board = [[str(i) for i in range(j*3), (j+1)*3] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
 
-##what are the available moves after you make a move
-#enumerate assigns a list and creates tuples
-def available_moves(self):
-    return [i for i, spot in enumerate(self.board) if spot == ' ']
-    # moves = []
-    # for (i, spot) in enumerate(self.board):
-    #     # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'o')]
-    #     if spot == ' ':
-    #         moves.append(i)
-    # return moves
+    ##what are the available moves after you make a move
+    #enumerate assigns a list and creates tuples
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def make_move(self, square, letter):
+        # if valid move, then make the move (assign square to letter)
+        # then return true. If invalid, return false
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
+    def winner(self, square, letter):
+        # winner is 3 in a row anywhere. We have to check all possibilities
+        # check the row
+        # how many times does 3 go into square
+        row_ind = square // 3
+        row = self.board[row_ind*3 : (row_ind + 1) * 3]
+        # check every spot in a row to see if the letter = the spot
+        if all([spot == letter for spot in row])
+            return True
+
+        #check column
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column])
+            return True
+
+        #check diagonal
+        
+
+
+def play(game, x_player, o_player, print_game=True):
+    # returns the winner of the game(the letter) or None for a tie
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X' # starting letter
+    # iterate while the still has empty squares
+    while game.empty_squares():
+        # get the move from the appropriaet player
+        if letter == 'O'
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+
+        #function to make a move
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f' makes a move to square {square}')
+                game.print_board()
+                print('') #empty line
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter
+
+            # after we made our move, we need to alternate letters
+            letter = 'O' if letter == 'X' else 'X'
+
+        if print_game:
+            print('It\'s a tie!')  
+    
+   
